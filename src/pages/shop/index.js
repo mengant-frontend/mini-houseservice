84c1e62e5b9d9adceea89b53265642f827f81a14
lifuzhao100 <lifuzhao100@gmail.com>
2018-10-23 21:47:36 +0800

    fix

--- src/pages/shop/index.js
+++ src/pages/shop/index.js
@@ -1,3 +1,31 @@
+import regeneratorRuntime from '../../lib/runtime'
+const app = getApp()
 Page({
-
+  data: {
+    detail: {}
+  },
+  async onPullDownRefresh() {
+    await this.loadData()
+    wx.stopPullDownRefresh()
+  },
+  async loadData() {
+    await app.asyncApi(wx.showNavigationLoading)
+    let server_res = await app.get({
+      url: '/api/v1/shop/info/edit'
+    })
+    await app.asyncApi(wx.hideNavigationLoading)
+    let { success, msg, data } = server_res
+    if (!success) {
+      app._error(msg)
+      return
+    }
+    this.setData({
+      detail: data
+    })
+  },
+  goToEdit() {
+    wx.navigateTo({
+      url: '/pages/shop/edit'
+    })
+  }
 })
